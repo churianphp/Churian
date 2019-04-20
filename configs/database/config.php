2e529dffb3c8ee48207e380b80942502cb9f4bd5
Stanley <dahumbleprof@gmail.com>
2019-04-20 10:56:06 +0100

    Change DB Username

--- configs/database/config.php
+++ configs/database/config.php
@@ -3,7 +3,7 @@
 # Define Constants
 # Configure For Database
 
-define("DBUSERNAME", "id9348270_churiandb");
+define("DBUSERNAME", "id9348270_churiandbadmin");
 define("DBNAME", "id9348270_churiandb");
 define("DBPASS", "#ChurianDBAdmin007");
 define("DBHOST", "localhost");
